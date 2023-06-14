8f50d63eb818103bd8eed8be02addf0e2421b903
praba <prabakaranmuthukumar08@gmail.com>
2023-06-14 10:39:22 +0530

    Feature-interactive mobileView Menu

---
+++ Assets/js/main.js
@@ -0,0 +1,15 @@
+const navToggler = document.querySelector(".nav-toggler");
+
+navToggler.addEventListener("click",() => {
+ const headerMenu = document.querySelector("ul.header--menu");
+    const navIcon = document.querySelectorAll(".navIcon");
+  
+
+    // Show menu
+    headerMenu.classList.toggle('show');
+
+    navIcon.forEach(icon =>{
+        icon.classList.toggle("hidden")
+    });
+});
+ 
